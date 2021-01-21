b3e3c44a4884c354835fe0cf9e363e5eb493e01c
Morgane Mottey <bob@example.com>
2021-01-21 16:54:04 +0100

    creation of a second view to modify our form

--- src/Controller/AccountAdressController.php
+++ src/Controller/AccountAdressController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Adress;
+use App\Form\AdressType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +16,17 @@ class AccountAdressController extends AbstractController
     public function index(): Response
     {
         return $this->render('account/address.html.twig', );
+    }
+    /**
+     * @Route("/compte/ajouter-une-adresse", name="add_account_adress")
+     */
+    public function add()
+    {
+        $adress = new Adress();
+
+        $form = $this->createForm(AdressType::class, $adress);
+        return $this->render('account/address_add.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/AdressType.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Adress;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class , [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom'
+                ]
+            ])
+            ->add('firstname', TextType::class , [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom'
+                ]
+            ])
+            ->add('lastname' , TextType::class , [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Votre nom'
+                ]
+            ])
+            ->add('company', TextType::class , [
+                'label' => 'Votre société',
+                'attr' => [
+                    'placeholder' => '(facultatif) Entrez le nom de votre société'
+                ]
+            ])
+            ->add('adress', TextType::class , [
+                'label' => 'Votre adresse',
+                'attr' => [
+                    'placeholder' => '8 rue des lilas..'
+                ]
+            ])
+            ->add('codepostal', TextType::class , [
+                'label' => 'Votre code postal',
+                'attr' => [
+                    'placeholder' => 'Entrez votre code postal'
+                ]
+            ])
+            ->add('city', TextType::class , [
+                'label' => 'Votre ville',
+                'attr' => [
+                    'placeholder' => 'Entrez votre ville'
+                ]
+            ])
+            ->add('phone', TelType::class , [
+                'label' => 'Votre téléphone',
+                'attr' => [
+                    'placeholder' => 'Entrez votre numéro'
+                ]
+            ])
+            ->add('submit' , SubmitType::class, [
+                'label' => 'Ajouter mon adresse'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Adress::class,
+        ]);
+    }
+}
